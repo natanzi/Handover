a73598479db0f9f37418653cd5ef4e5cab2f5ba7
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-05 20:23:27 -0500

    Update handover_server.h

--- handover_server.h
+++ handover_server.h
@@ -1,28 +1,40 @@
 // handover_server.h // Edited by Milad
+
 #ifndef HANDOVER_SERVER_H
 #define HANDOVER_SERVER_H
 
 #include <atomic>
 #include <thread>
 #include <vector>
+#include <mutex>
+
+// Assume SRSRAN_SUCCESS and SRSRAN_ERROR are defined somewhere, e.g.:
+#ifndef SRSRAN_SUCCESS
+#define SRSRAN_SUCCESS 0
+#endif
+
+#ifndef SRSRAN_ERROR
+#define SRSRAN_ERROR -1
+#endif
 
 class HandoverServer {
 public:
     explicit HandoverServer(int port);
     ~HandoverServer();
     
-    bool start();
+    int start();
     void stop();
 
 private:
-    static void server_loop();
-    static void handle_client(int client_socket); // Now declared static
+    void server_loop();
+    void handle_client(int client_socket);
 
     int server_fd{-1};
     int port;
     std::atomic<bool> is_running{false};
     std::thread server_thread;
     std::vector<std::thread> client_threads;
+    std::mutex client_threads_mutex;
 };
 
 #endif // HANDOVER_SERVER_H

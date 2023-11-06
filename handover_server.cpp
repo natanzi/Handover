fecaf760baef1b54dc5c3c8993f48a3507f55275
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-05 20:40:56 -0500

    Update handover_server.cpp

--- handover_server.cpp
+++ handover_server.cpp
@@ -6,99 +6,77 @@
 #include <iostream>
 #include <cstring>
 #include <unistd.h>
+#include <string>
 
+// Constructor
 HandoverServer::HandoverServer(int port) : port(port), is_running(false), server_fd(-1) {}
 
+// Destructor
 HandoverServer::~HandoverServer() {
     stop();
 }
 
+// Start the server
 int HandoverServer::start() {
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        std::cerr << "Failed to create socket: " << strerror(errno) << std::endl;
-        return SRSRAN_ERROR;
-    }
-
-    struct sockaddr_in server_addr;
-    std::memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(this->port);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Failed to bind socket to port " << this->port << ": " << strerror(errno) << std::endl;
-        close(server_fd);
-        return SRSRAN_ERROR;
-    }
-
-    if (listen(server_fd, 10) < 0) {
-        std::cerr << "Failed to listen on socket: " << strerror(errno) << std::endl;
-        close(server_fd);
-        return SRSRAN_ERROR;
-    }
+    // ... existing start code ...
 
     is_running = true;
     server_thread = std::thread(&HandoverServer::server_loop, this);
     return SRSRAN_SUCCESS;
 }
 
+// Stop the server
 void HandoverServer::stop() {
-    is_running = false;
-    if (server_thread.joinable()) {
-        server_thread.join();
-    }
-
-    std::lock_guard<std::mutex> lock(client_threads_mutex);
-    for (auto& thread : client_threads) {
-        if (thread.joinable()) {
-            thread.join();
-        }
-    }
-
-    if (server_fd != -1) {
-        close(server_fd);
-    }
+    // ... existing stop code ...
 }
 
+// Server loop to accept clients
 void HandoverServer::server_loop() {
-    while (is_running.load()) {
-        struct sockaddr_in client_addr;
-        socklen_t client_len = sizeof(client_addr);
-        int client_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
-        if (client_socket < 0) {
-            if (!is_running.load()) {
-                break;
-            }
-            std::cerr << "Failed to accept client connection: " << strerror(errno) << std::endl;
-            continue;
-        }
-
-        client_threads.emplace_back(&HandoverServer::handle_client, this, client_socket);
-    }
+    // ... existing server_loop code ...
 }
 
+// Handle client connections
 void HandoverServer::handle_client(int client_socket) {
-    char buffer[1024] = {0};
+    const int buffer_size = 1024;
+    char buffer[buffer_size] = {0};
     std::string data;
+    std::string keep_alive_msg = "KEEP_ALIVE\n";
+    std::string ack_msg = "ACK\n";
 
-    ssize_t bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-    if (bytes_received <= 0) {
-        close(client_socket);
-        return;
+    while (is_running.load()) {
+        // Clear the buffer
+        std::memset(buffer, 0, buffer_size);
+
+        // Receive data from the client
+        ssize_t bytes_received = recv(client_socket, buffer, buffer_size, 0);
+
+        // Check for disconnection or errors
+        if (bytes_received <= 0) {
+            std::cerr << "Client disconnected or error occurred.\n";
+            break; // Exit the loop and close the socket
+        }
+
+        // Convert buffer to a std::string for easier handling
+        data = std::string(buffer, bytes_received);
+
+        // Check if this is a keep-alive message
+        if (data == keep_alive_msg) {
+            // Send an acknowledgment back to the client
+            send(client_socket, ack_msg.c_str(), ack_msg.size(), 0);
+        } else {
+            // Handle other messages
+            std::cout << "Received message: " << data << std::endl;
+
+            // Assume any other message is a handover command and process it
+            std::cout << "Command received, Going to apply handover...: " << data << std::endl;
+
+            // Send a success response back to the client
+            std::string response = "Handover acknowledged.\n";
+            send(client_socket, response.c_str(), response.size(), 0);
+        }
     }
 
-    data = std::string(buffer, bytes_received);
-
-    size_t newline_pos = data.find('\n');
-    if (newline_pos != std::string::npos) {
-        data.resize(newline_pos);
-    }
-
-    std::cout << "Command received, Going to apply handover...: " << data << std::endl;
-
-    std::string response = "Handover acknowledged.\n";
-    send(client_socket, response.c_str(), response.size(), 0);
-
+    // If we're here, the loop has ended, so we should clean up and close the socket
     close(client_socket);
 }
+

4aa7b40d20bee22b2b4e70ee3d0ef1d46c9dd0b0
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-05 21:09:13 -0500

    Update handover_server.cpp

--- handover_server.cpp
+++ handover_server.cpp
@@ -1,5 +1,4 @@
 // handover_server.cpp edited by Milad
-
 #include "handover_server.h"
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -7,6 +6,7 @@
 #include <cstring>
 #include <unistd.h>
 #include <string>
+#include <thread>
 
 // Constructor
 HandoverServer::HandoverServer(int port) : port(port), is_running(false), server_fd(-1) {}
@@ -18,7 +18,36 @@ HandoverServer::~HandoverServer() {
 
 // Start the server
 int HandoverServer::start() {
-    // ... existing start code ...
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        std::cerr << "Failed to create socket: " << strerror(errno) << std::endl;
+        return SRSRAN_ERROR;
+    }
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        std::cerr << "Failed to set socket options: " << strerror(errno) << std::endl;
+        close(server_fd);
+        return SRSRAN_ERROR;
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Failed to bind socket to port " << port << ": " << strerror(errno) << std::endl;
+        close(server_fd);
+        return SRSRAN_ERROR;
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        std::cerr << "Failed to listen on socket: " << strerror(errno) << std::endl;
+        close(server_fd);
+        return SRSRAN_ERROR;
+    }
 
     is_running = true;
     server_thread = std::thread(&HandoverServer::server_loop, this);
@@ -27,12 +56,33 @@ int HandoverServer::start() {
 
 // Stop the server
 void HandoverServer::stop() {
-    // ... existing stop code ...
+    is_running = false;
+    if (server_thread.joinable()) {
+        server_thread.join();
+    }
+    if (server_fd != -1) {
+        close(server_fd);
+    }
 }
 
 // Server loop to accept clients
 void HandoverServer::server_loop() {
-    // ... existing server_loop code ...
+    while (is_running.load()) {
+        struct sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
+        int client_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+        if (client_socket < 0) {
+            std::cerr << "Failed to accept client connection: " << strerror(errno) << std::endl;
+            if (!is_running.load()) {
+                break; // Server is stopping
+            }
+            continue; // Try to accept another connection
+        }
+
+        // Handle the client connection in a separate thread or function
+        std::thread client_thread(&HandoverServer::handle_client, this, client_socket);
+        client_thread.detach(); // Detach the thread to handle multiple clients
+    }
 }
 
 // Handle client connections

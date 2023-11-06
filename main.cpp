97261982b87bef9ef96ac8058b732e28c150f940
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-05 20:29:47 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// main.cpp
+
+#include "handover_server.h"
+#include <iostream>
+
+int main() {
+    int port = 12345; // Replace with the port number you want the server to listen on
+    HandoverServer server(port);
+
+    if (server.start() != SRSRAN_SUCCESS) {
+        std::cerr << "Failed to start server on port " << port << std::endl;
+        return 1; // Return a non-zero value to indicate an error
+    }
+
+    std::cout << "Server started on port " << port << ". Press enter to stop the server..." << std::endl;
+    std::cin.get(); // Wait for user input to stop the server
+
+    server.stop();
+    std::cout << "Server stopped." << std::endl;
+
+    return 0; // Successful execution
+}

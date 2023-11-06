3ba88f5346fcce372d62f6296942988c27dcda5d
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-05 20:14:14 -0500

    Update handover_server.cpp

--- handover_server.cpp
+++ handover_server.cpp
@@ -12,39 +12,22 @@
 
 // Assume SRSRAN_SUCCESS and SRSRAN_ERROR are defined somewhere, e.g.:
 #ifndef SRSRAN_SUCCESS
-#define SRSRAN_SUCCESS 0
+#define SRSRAN_SUCCESS true
 #endif
 
 #ifndef SRSRAN_ERROR
-#define SRSRAN_ERROR -1
+#define SRSRAN_ERROR false
 #endif
 
-class HandoverServer {
-public:
-    HandoverServer(int port);
-    ~HandoverServer();
-
-    int start();
-    void stop();
-
-private:
-    void server_loop();
-    static void handle_client(int client_socket);
-
-    int port;
-    bool is_running;
-    int server_fd;
-    std::thread server_thread;
-    std::vector<std::thread> client_threads;
-    std::mutex client_threads_mutex;
-};
-
+// Constructor implementation
 HandoverServer::HandoverServer(int port) : port(port), is_running(false), server_fd(-1) {}
 
+// Destructor implementation
 HandoverServer::~HandoverServer() {
     stop();
 }
 
+// Start method implementation
 bool HandoverServer::start() {
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
@@ -53,13 +36,13 @@ bool HandoverServer::start() {
     }
 
     struct sockaddr_in server_addr;
-    std::memset(&server_addr, 0, sizeof(server_addr));
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
+    server_addr.sin_port = htons(this->port);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Failed to bind socket to port " << port << ": " << strerror(errno) << std::endl;
+        std::cerr << "Failed to bind socket to port " << this->port << ": " << strerror(errno) << std::endl;
         close(server_fd);
         return SRSRAN_ERROR;
     }
@@ -71,88 +54,69 @@ bool HandoverServer::start() {
     }
 
     is_running = true;
-    try {
-        server_thread = std::thread(&HandoverServer::server_loop, this);
-    } catch (const std::exception& e) {
-        std::cerr << "Failed to create server thread: " << e.what() << std::endl;
-        close(server_fd);
-        return SRSRAN_ERROR;
-    }
+    server_thread = std::thread(&HandoverServer::server_loop, this);
+
     return SRSRAN_SUCCESS;
 }
 
+// Stop method implementation
 void HandoverServer::stop() {
     is_running = false;
     if (server_thread.joinable()) {
         server_thread.join();
     }
 
-    {
-        std::lock_guard<std::mutex> lock(client_threads_mutex);
-        for (auto& thread : client_threads) {
-            if (thread.joinable()) {
-                thread.join();
-            }
+    std::lock_guard<std::mutex> lock(client_threads_mutex);
+    for (auto& thread : client_threads) {
+        if (thread.joinable()) {
+            thread.join();
         }
     }
+    client_threads.clear();
 
     if (server_fd != -1) {
         close(server_fd);
     }
 }
 
+// Server loop method implementation
 void HandoverServer::server_loop() {
-    while (is_running) {
+    while (is_running.load()) {
         struct sockaddr_in client_addr;
         socklen_t client_len = sizeof(client_addr);
         int client_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
         if (client_socket < 0) {
-            if (!is_running) { // If the server is stopped, it's normal for accept to fail
+            if (!is_running.load()) {
                 break;
             }
             std::cerr << "Failed to accept client connection: " << strerror(errno) << std::endl;
             continue;
         }
 
-        try {
-            std::lock_guard<std::mutex> lock(client_threads_mutex);
-            client_threads.emplace_back([this](int sock){ handle_client(sock); }, client_socket);
-        } catch (const std::exception& e) {
-            std::cerr << "Failed to create client thread: " << e.what() << std::endl;
-            close(client_socket);
-        }
+        client_threads.emplace_back(&HandoverServer::handle_client, this, client_socket);
     }
 }
 
+// Handle client method implementation
 void HandoverServer::handle_client(int client_socket) {
     char buffer[1024] = {0};
-    std::string data;
 
-    // Read data from the socket
     ssize_t bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
     if (bytes_received <= 0) {
-        // Handle error or disconnection
         close(client_socket);
         return;
     }
 
-    // Convert buffer to a std::string for easier handling
-    data = std::string(buffer, bytes_received);
-
-    // Look for the newline character and trim the data up to that point
+    std::string data(buffer, bytes_received);
     size_t newline_pos = data.find('\n');
     if (newline_pos != std::string::npos) {
-        data.resize(newline_pos); // Removes the newline character and any data after it
+        data.resize(newline_pos);
     }
 
-    // Assuming the command is always correct, we'll just log the command
-    std::cout << "Command received, Going to apply handover...: " << data << std::endl;
+    std::cout << "Command received: " << data << std::endl;
 
-    // Send a success response back to the client
     std::string response = "Handover acknowledged.\n";
     send(client_socket, response.c_str(), response.size(), 0);
 
-    // Close the socket for this client
     close(client_socket);
 }
-
